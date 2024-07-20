2ed89a32b5af27a9218a78c6172bde806244fee5
Noah Doersing <git@noahdoersing.com>
2024-07-20 11:39:26 +0200

    Add code

---
+++ Control.Zoom2Markers.js
@@ -0,0 +1,114 @@
+/*
+ * leaflet.zoom2markers
+ * (c) Noah Doersing; MIT License
+ * Uses fragments from the package 'leaflet.fullscreen' by Bruno B.
+ */
+(function(root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		// define an AMD module that requires 'leaflet'
+		// and resolve to an object containing leaflet
+		define('leafletZoom2Markers', ['leaflet'], factory);
+	} else if (typeof module === 'object' && module.exports) {
+		// define a CommonJS module that requires 'leaflet'
+		module.exports = factory(require('leaflet'));
+	} else {
+		// Assume 'leaflet' are loaded into global variable already
+		factory(root.L);
+	}
+}(typeof self !== 'undefined'
+	? self
+	: this, (leaflet) => {
+	'use strict';
+
+	if (typeof document === 'undefined') {
+		console.warn('"window.document" is undefined; leaflet.zoom2markers requires this object to access the DOM');
+		return false;
+	}
+
+	leaflet.Control.Zoom2Markers = leaflet.Control.extend({
+		options: {
+			position: 'topleft',
+			title: 'Zoom to Markers',
+			forceSeparateButton: false,  // separate from +/- zoom controls?
+		},
+
+		onAdd(map) {
+			let className = 'leaflet-control-zoom-zoom2markers';
+			let container;
+			let content = '';
+
+			if (map.zoomControl && !this.options.forceSeparateButton) {
+				container = map.zoomControl._container;
+			} else {
+				container = leaflet.DomUtil.create('div', 'leaflet-bar');
+			}
+
+			if (this.options.content) {
+				content = this.options.content;
+			} else {
+				className += ' zoom2markers-icon';
+			}
+
+			this._createButton(this.options.title, className, content, container, this.doZoom2Markers, this);
+			this._map.zoom2MarkersControl = this;
+
+			return container;
+		},
+
+		onRemove() {
+			leaflet.DomEvent
+				.off(this.link, 'click', leaflet.DomEvent.stop)
+				.off(this.link, 'click', this.doZoom2Markers, this);
+		},
+
+		// based on https://stackoverflow.com/questions/68020915/leaflet-js-zoom-map-to-visible-markers
+		doZoom2Markers() {
+
+			// collect non-excluded markers into a feature group
+			const markerFeatureGroup = new leaflet.FeatureGroup();
+			this._map.eachLayer(function(layer) {
+				if (layer instanceof leaflet.Marker && !layer.options.zoom2MarkersExclude) {
+					markerFeatureGroup.addLayer(layer);
+				}
+			});
+
+			// unless empty...
+			if (markerFeatureGroup.getLayers().length > 0) {
+
+				// ...get bounds and zoom to them
+				const bounds = markerFeatureGroup.getBounds();
+				this._map.fitBounds(bounds);
+			}
+		},
+
+		_createButton(title, className, content, container, fn, context) {
+			this.link = leaflet.DomUtil.create('a', className, container);
+			this.link.href = '#';
+			this.link.title = title;
+			this.link.innerHTML = content;
+
+			this.link.setAttribute('role', 'button');
+			this.link.setAttribute('aria-label', title);
+
+			leaflet.DomEvent.disableClickPropagation(container);
+
+			leaflet.DomEvent
+				.on(this.link, 'click', leaflet.DomEvent.stop)
+				.on(this.link, 'click', fn, context);
+
+			return this.link;
+		}
+	});
+
+	leaflet.Map.addInitHook(function() {
+		if (this.options.zoom2MarkersControl) {
+			this.addControl(leaflet.control.zoom2markers(this.options.zoom2MarkersControlOptions));
+		}
+	});
+
+	leaflet.control.zoom2markers = function(options) {
+		return new leaflet.Control.Zoom2Markers(options);
+	};
+
+	return { leaflet };
+}));
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# leaflet-zoom2markers
-Basic Leaflet control that zooms the map to show all markers.
+# Leaflet.Control.Zoom2Markers
+
+A basic [Leaflet](https://leafletjs.com) control that, when clicked, zooms the map to show all markers.
+
+In lieu of a demo (because this plugin is so dead simple), here's a screenshot – it's the bottom-most button.
+
+
+
+## Setup
+
+Download or clone this repository, then reference the JavaScript code and stylesheet in your HTML:
+
+```html
+<link rel="stylesheet" href="Control.Zoom2Markers.css">
+<script src="Control.Zoom2Markers.js"></script>
+```
+
+
+## Usage
+
+After initializing your map:
+
+```js
+L.control.zoom2markers({
+    position: 'topleft',
+    title: 'Zoom to Markers',
+    forceSeparateButton: false,  // separate from +/- zoom controls?
+}).addTo(map);
+```
+
+(These are the default options, you can omit them unless you're going to assign different values.)
+
+You can exclude markers from consideration by setting `zoom2MarkersExclude: true` in *their* options; the zoom action will then ignore them.
+
+
+## Notes
+
+Not tested for "interactions" (as in, drug interactions) with other extensions. (This one just scoops up all layers of type `L.Marker` into a `FeatureGroup`, then `fitBounds` the map to that group's `getBounds()`.)
+
+
+## Acknowledgements
+
+The icon is MIT Licensed and © [Etn Ccis](https://github.com/etn-ccis/blui-icons?ref=svgrepo.com).
+
+The code is based on [Bruno Bergot's "leaflet.fullscreen" extension](https://github.com/brunob/leaflet.fullscreen).
